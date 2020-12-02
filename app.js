43762ba21f98e3805ef4bcb1dafc204278f60904
unknown <amit.singh.bla@gmail.com>
2020-12-02 22:13:08 +0530

    Url missing given a message error 404

--- app.js
+++ app.js
@@ -10,12 +10,21 @@ mongoose.connect("mongodb://localhost:27017/userdb",{useNewUrlParser:true,useUni
 const user=require("./models/users");
 
 app.get("/",(req,res)=>{
+        res.send("welcome to the home page");
+})
+
+app.get("/users/api",(req,res)=>{
     user.find({}).then( (data)=>{
         res.send(data);
         console.log(data);
     })
-   // res.send("welcome");
 })
+
+
+app.get("*",(req,res)=>{
+    res.send("error 404 found",404);
+})
+
 app.listen(port,(req,res)=>{
     console.log(`Listening at the port ${port}`);
 })
